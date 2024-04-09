3f0d2abd4b7ec153b15b8f5ba43888ee0c258e21
pavan-reddy-28 <saipavanreddyr21@gmail.com>
2024-04-09 08:51:24 -0700

    new docker builder

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = 'https://df184qz7f6.execute-api.us-east-2.amazonaws.com'
 
 export const SECRET='ag4B24lVui0WOrKETOEjh6wMfKf5T/fjMWvndm7eRqkscRwM6f+aQVzGIt1BuL9A'
-export const APP_URL='/client-app-pep'
+export const APP_URL='/client-app-pep' 
